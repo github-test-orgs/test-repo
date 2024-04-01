6ff64c508403aeb77d4e6459f972d25a1b212454
Ismael Rekik <65619960+IRekik@users.noreply.github.com>
2024-04-01 16:17:37 +0900

    Update redeploy.sh (#54)

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,6 @@
-echo "REACH"
-git reset --hard
-git pull origin master
-npm i
-npm run build
+sudo git reset --hard
+sudo git pull origin master
+sudo npm i
+sudo npm run build
 sudo lsof -ti tcp:3000 | xargs sudo kill -9 # Kills process on port 3000
-npm run dev
+sudo npm run dev
