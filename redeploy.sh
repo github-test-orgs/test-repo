a7e15f284b723942753d5fdc92fa08f86e0d0f6b
Ismael Rekik <65619960+IRekik@users.noreply.github.com>
2024-04-04 16:06:47 +0900

    Build files can now be logged, along with the build command logs, for each execution (#85)

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 sudo git reset --hard
 sudo git pull origin master
 sudo npm i
-npm run build 2>&1 | tee "build_$(date +%Y-%m-%d_%H-%M-%S).log"
+npm run build 2>&1 | tee "build.log"
 sudo lsof -ti tcp:3000 | xargs sudo kill -9 # Kills process on port 3000
 sudo npm run dev
